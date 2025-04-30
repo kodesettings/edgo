a916bbbe30ca2a0db350384165ea8b139a2b7bd4
Marcell Nemes <marcellnemes98@gmail.com>
2025-04-30 05:20:10 +0200

    Added csharp and scala language grammars support too

--- internal/highlighter/tree_sitter_highlighter.go
+++ internal/highlighter/tree_sitter_highlighter.go
@@ -9,12 +9,14 @@ import (
 	"github.com/smacker/go-tree-sitter/bash"
 	"github.com/smacker/go-tree-sitter/c"
 	"github.com/smacker/go-tree-sitter/cpp"
+	"github.com/smacker/go-tree-sitter/csharp"
 	"github.com/smacker/go-tree-sitter/golang"
 	"github.com/smacker/go-tree-sitter/html"
 	"github.com/smacker/go-tree-sitter/java"
 	"github.com/smacker/go-tree-sitter/javascript"
 	"github.com/smacker/go-tree-sitter/lua"
 	"github.com/smacker/go-tree-sitter/kotlin"
+	"github.com/smacker/go-tree-sitter/scala"
 	"github.com/smacker/go-tree-sitter/python"
 	"github.com/smacker/go-tree-sitter/rust"
 	"github.com/smacker/go-tree-sitter/typescript/typescript"
@@ -116,8 +118,10 @@ func GetSitterLang(lang string) *sitter.Language {
 	case "c": return c.GetLanguage()
 	case "c++": return cpp.GetLanguage()
 	case "cpp": return cpp.GetLanguage()
+	case "csharp": return csharp.GetLanguage()
 	case "html": return html.GetLanguage()
 	case "kotlin": return kotlin.GetLanguage()
+	case "scala": return scala.GetLanguage()
 	case "yaml": return yaml.GetLanguage()
 	case "java":return java.GetLanguage()
 	case "bash": return bash.GetLanguage()
