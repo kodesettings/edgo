722bdc8520830e59db3f4cfe687e8d6671387a09
Marcell Nemes <marcellnemes98@gmail.com>
2025-07-31 03:47:15 +0300

    Updated and cleaned up highlighter implementation

--- internal/highlighter/highlighter.go
+++ internal/highlighter/highlighter.go
@@ -17,17 +17,6 @@ type Highlighter struct {
 
 }
 
-var theme = Vesper
-
-//var theme = styles.Get("edgo")
-//var theme = styles.Get("dracula")
-//var theme = styles.Get("nord")
-//var theme = styles.Get("monokai")
-//var theme = styles.Get("paraiso-dark")
-//var theme = styles.Get("vulcan")
-//var theme = styles.Get("witchhazel")
-//var theme = styles.Get("xcode-dark")
-
 var SelectionColor = 246 // gray
 var HighlightColor = 238 // gray
 var OverlayColor = -1    // transparent
@@ -41,14 +30,6 @@ var DimmedStyle = tcell.StyleDefault.Foreground(tcell.ColorDimGray)
 func ResetSelectionColor() {
 	SeparatorStyle = tcell.StyleDefault.Foreground(tcell.ColorDimGray)
 }
-
-//func DetectLang(filename string) string {
-//	lexer := lexers.Match(filename)
-//	if lexer == nil { return "" }
-//	config := lexer.Config()
-//	if config == nil { return "" }
-//	return strings.ToLower(config.Name)
-//}
 
 func DetectLang(filename string) string {
 	language, _ := enry.GetLanguageByExtension(filename)
@@ -64,17 +45,10 @@ func DetectLang(filename string) string {
 	if language == "renderscript" { return "rust" }
 
 	return language
-
-	//info, _ := enry.GetLanguageInfo(language)
-	//
-	//codeMirrorMode := info.AceMode
-	//if codeMirrorMode == "" { return "text" }
-	//
-	//return strings.ToLower(codeMirrorMode)
 }
 
 func (h *Highlighter) SetTheme(name string) {
-	theme = styles.Get(name)
+	theme := styles.Get(name)
 	AccentColor = int(tcell.GetColor(theme.Get(chroma.Keyword).Colour.String()))
 	AccentColor2 = int(tcell.GetColor(theme.Get(chroma.KeywordType).Colour.String()))
 }
@@ -88,12 +62,12 @@ func (h *Highlighter) GetRunButtonStyle() int {
 	return int(tcell.GetColor("#90EE90"))
 }
 
+var Theme = Vesper
+
 func (h *Highlighter) Colorize(code string, filename string) [][]int {
 	if code == "" {
 		return [][]int{nil}
 	}
-
-	//start := time.Now()
 
 	// get lexer depending on Name
 	lexer := lexers.Match(filename)
@@ -114,9 +88,10 @@ func (h *Highlighter) Colorize(code string, filename string) [][]int {
 	for _, tokens := range tokensIntoLines {
 		lineColors := []int{}
 		for _, token := range tokens {
-			chromaColor := theme.Get(token.Type).Colour.String()
+			chromaColor := Theme.Get(token.Type).Colour.String()
 			tcellColor := tcell.GetColor(chromaColor)
 			color := int(tcellColor)
+
 			// copy color for each token character
 			for range token.Value {
 				lineColors = append(lineColors, color)
@@ -125,6 +100,5 @@ func (h *Highlighter) Colorize(code string, filename string) [][]int {
 		textColors = append(textColors, lineColors)
 	}
 
-	//Log.Info("colorize end, elapsed: " + time.Since(start).String())
 	return textColors
 }
