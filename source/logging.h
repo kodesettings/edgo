a114bcda1809a345ab685d547079e8d7c8f54f2e
Marcell Nemes <marcellnemes98@gmail.com>
2026-01-14 21:53:50 +0200

    Fixed logging header due to gcc upgrade

--- source/logging.h
+++ source/logging.h
@@ -15,14 +15,10 @@
     with this program; if not, see <https://www.gnu.org/licenses/>.
 */
 
-#ifndef _LOGGING_H_
-#define _LOGGING_H_
-
+#pragma once
 #include <glog/logging.h>
 
 // Initialization macros for google's logging system.
 // By default we have to log into files on disk.
 #define EDGO_LOGGING_INIT google::InitGoogleLogging("edgo");
 #define EDGO_LOGGING_TO_FILE FLAGS_logtostderr = 0;
-
-#endif // _LOGGING_H_
