6561967ee6141559a5b52855e5e8408b3c5b5cd9
Marcell Nemes <marcellnemes98@gmail.com>
2026-08-25 21:49:24 +0300

    Updated screen report layout

--- source/screen.cpp
+++ source/screen.cpp
@@ -36,6 +36,7 @@ std::string OnLangLinesCount(const std::string &dirpath) {
 	LangCount(files, &results);
 
 	std::stringstream report;
+	report << std::endl << std::endl << std::endl << std::endl;
 	report << "      " << "Language and File Statistics Report" << std::endl;
 	report << std::endl;
 	report << LONG_W << "Total Files : " << SHORT_W << fpc << std::endl;
